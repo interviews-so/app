841da676fb378091c0a7bd4ddd8bc1f4071e20b6
Andrew Henry <andrew@ajhenry.dev>
2024-01-21 00:51:18 -0500

    fix: logo on login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -37,7 +37,7 @@ export default async function LoginPage() {
       </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <div className="mx-auto h-6 w-6">
+          <div className="mx-auto">
             <Logo />
           </div>
           <h1 className="text-2xl font-semibold tracking-tight">
